48b0a977939f50428b50276fedf5dc97743b3c29
Renzo Perez <renzin@gmail.com>
2021-05-27 10:27:20 -0500

    adding helpers and light

--- modules.js
+++ modules.js
@@ -1,13 +1,33 @@
-import * as THREE from "./tjs/three.module.js"
-import { OrbitControls } from "./tjs/OrbitControls.js"
+import * as THREE from "./tjs/three.module.js" //The main module
+import { OrbitControls } from "./tjs/OrbitControls.js" //This will help us control the camera
+
+/*
+We need at least these 3 objects to actually see something
+Scene:    Container that holds all your objects, cameras and lights
+Camera:   Indicates what part of the scene will the focus of attention
+          PerspectiveCamera(FOV,aspect ration(browser window), view frustum(near, far))
+Renderer: Render the graphics to the scenes (the canvas)
+*/
+
 var scene = new THREE.Scene();
-scene.background = new THREE.Color(0x2a3b4c);
+//scene.background = new THREE.Color(0x2a3b4c);
 var camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
+/*
+To Create an object we need 3 things
+Geometry: Set of vectors that define the vector itself
+          Look at https://threejsfundamentals.org/threejs/lessons/threejs-primitives.html
+          for more info
+Material: The wrapping paper for an object
+          Look at https://threejs.org/docs/?q=mate#api/en/materials/Material
+          for more info
+Mesh:     Geometry+Material
+*/
+// Torus
 var geometry = new THREE.TorusGeometry(20, 2, 16, 100);
 //MeshBasicMaterial requires no light source
 var material = new THREE.MeshBasicMaterial({ color: 0x5f11dd, wireframe: true});
@@ -20,7 +40,14 @@ camera.position.setZ(50);/*Objects will spawn in the middle so we move the camer
 var gridHelper = new THREE.GridHelper(200, 50);
 scene.add(gridHelper);
 
-//const controls = new Three.OrbitControls(camera, renderer.domElement)
+const pointLight = new THREE.PointLight(0xffffff);//Acts as a bulb in the scene
+pointLight.position.set(10, 10, 10);
+
+scene.add(pointLight);
+const lightHelper = new THREE.PointLightHelper(pointLight)//This will help you locate the position of the pl
+scene.add(lightHelper);
+
+
 var controls = new OrbitControls(camera, renderer.domElement)
 
 renderer.render(scene, camera);
@@ -32,6 +59,7 @@ var animate = function(){
     torus.rotation.x += 0.025;
     torus.rotation.y += 0.025;
     torus.rotation.z += 0.025;
+
 	// required if controls.enableDamping or controls.autoRotate are set to true
 	//controls.update();
 
